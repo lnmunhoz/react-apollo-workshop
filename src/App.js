f61c8ac067db2087837c30d64d4240316f0e6bd7
Lucas Munhoz <ln.munhoz@gmail.com>
2019-04-09 12:45:11 +0700

    Listing issues of a repository

--- src/App.js
+++ src/App.js
@@ -1,8 +1,93 @@
 import React from "react";
 import Layout from "./Layout";
+import { Query } from "react-apollo";
+import { gql } from "apollo-boost";
+import {
+  ListItem,
+  ListItemAvatar,
+  Avatar,
+  Typography,
+  List,
+  ListItemText
+} from "@material-ui/core";
+
+const MY_QUERY = gql`
+  query {
+    viewer {
+      login
+    }
+  }
+`;
+
+const GET_REPOSITORY = gql`
+  query {
+    repository(owner: "lnmunhoz", name: "react-apollo-workshop") {
+      id
+      name
+      issues(first: 10) {
+        nodes {
+          id
+          title
+          bodyText
+          closed
+          author {
+            login
+            avatarUrl
+          }
+        }
+      }
+    }
+  }
+`;
+
+const MyUsername = () => (
+  <Query query={MY_QUERY}>
+    {({ data, loading }) => {
+      if (loading) return <p>Loading...</p>;
+      return <p>{data.viewer.login}</p>;
+    }}
+  </Query>
+);
+
+const Repository = () => (
+  <Query query={GET_REPOSITORY}>
+    {({ data, loading }) => {
+      if (loading) return <p>Loading...</p>;
+
+      const repository = data.repository;
+      const issues = repository.issues.nodes;
+
+      return (
+        <List>
+          {issues.map(issue => (
+            <ListItem alignItems="flex-start" key={issue.id}>
+              <ListItemAvatar>
+                <Avatar alt="Remy Sharp" src={issue.author.avatarUrl} />
+              </ListItemAvatar>
+              <ListItemText
+                primary={issue.title}
+                secondary={
+                  <React.Fragment>
+                    <Typography component="span" color="textPrimary">
+                      {issue.bodyText}
+                    </Typography>
+                  </React.Fragment>
+                }
+              />
+            </ListItem>
+          ))}
+        </List>
+      );
+    }}
+  </Query>
+);
 
 function App() {
-  return <Layout>Hello World</Layout>;
+  return (
+    <Layout>
+      <Repository />
+    </Layout>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,27 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+// 1. Import apollo-boost and react-apollo
+import { ApolloClient, HttpLink, InMemoryCache } from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// 2. Create a new apollo client
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: "https://api.github.com/graphql",
+    headers: {
+      authorization: "Bearer e92dd5da656aa917742135650cb866376558199f"
+    }
+  })
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
